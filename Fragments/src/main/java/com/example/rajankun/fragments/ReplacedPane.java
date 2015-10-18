5d4330470254f24e39d0eeaaa3628a48c9e013fd
unknown <rajannakul@gmail.com>
2015-10-18 17:02:11 -0500

    frament example added

--- Fragments/src/main/java/com/example/rajankun/fragments/ReplacedPane.java
+++ Fragments/src/main/java/com/example/rajankun/fragments/ReplacedPane.java
@@ -7,10 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class BottomPane extends Fragment{
+public class ReplacedPane extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.bottom_panel, container, false);
+        return inflater.inflate(R.layout.replaced_panel, container, false);
     }
 }
--- Fragments/src/main/java/com/example/rajankun/fragments/TopPane.java
+++ Fragments/src/main/java/com/example/rajankun/fragments/TopPane.java
@@ -1,7 +1,46 @@
 package com.example.rajankun.fragments;
 
-
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class TopPane extends Fragment{
+
+    OnButtonClickedListner mButtonClickedListner;
+
+    public interface OnButtonClickedListner {
+         void OnTopPanelButtonClicked();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view =  inflater.inflate(R.layout.top_panel, container, false);
+        Button button = (Button) view.findViewById(R.id.replace_panel);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mButtonClickedListner.OnTopPanelButtonClicked();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mButtonClickedListner = (OnButtonClickedListner) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
+    }
 }
